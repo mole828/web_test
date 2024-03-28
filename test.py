40da187f7555bdce0e3ea96df052f6b05734e598
mole828 <mrx800@163.com>
2024-03-28 21:00:49 +0800

    update

---
+++ test.py
@@ -0,0 +1,33 @@
+import asyncio
+from collections import Counter
+import json
+import time
+import requests
+
+counter = Counter()
+
+async def count():
+    resp = requests.get("http://10.0.0.42:8009/api/v1/namespaces/default/services/web-api-service:8080/proxy/count")
+    data = json.loads(resp.text)
+    count_num = data['count']
+    counter[count_num] += 1
+
+count_times = 1000
+
+async def main():
+    tasks = [count() for _ in range(count_times)]
+    await asyncio.gather(*tasks)
+    print(counter)
+
+# async def main():
+#     for i in range(count_times):
+#         await count()
+#     print(counter)
+
+if __name__ == '__main__': 
+    begin = time.time()
+    asyncio.run(main())
+    end = time.time() 
+    print(f"spend {end-begin}")
+
+    
